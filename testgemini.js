ed175c88b27be65d75eef6e581dc79a926506e63
devanshudutta <108408165+devanshudutta@users.noreply.github.com>
2025-09-30 13:21:15 +0530

    removed api key

--- testgemini.js
+++ testgemini.js
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({ apiKey: "AIzaSyCG7nVmiT3-DncmVW9EMW8JvzYhTLLSVFk" });
+const ai = new GoogleGenAI({ apiKey: "" });
 
 console.log("API KEY:", process.env.DISCORD_TOKEN);
 
